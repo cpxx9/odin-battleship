acb33438bff3ff762a772cb005c0acb166003025
CPxx9 <127376939+cpxx9@users.noreply.github.com>
2024-05-30 14:13:51 -0400

    fix: test now use length method instead of prop

--- src/js/tests/Ship.test.js
+++ src/js/tests/Ship.test.js
@@ -3,12 +3,12 @@ import Ship from '../modules/Ship';
 describe('instantiation tests', () => {
   test('creates a Ship with length of 3', () => {
     const sh1 = new Ship(3);
-    expect(sh1.length).toBe(3);
+    expect(sh1.length()).toBe(3);
   });
 
   test('creates a Ship with length of 5', () => {
     const sh2 = new Ship(5);
-    expect(sh2.length).toBe(5);
+    expect(sh2.length()).toBe(5);
   });
 });
 
