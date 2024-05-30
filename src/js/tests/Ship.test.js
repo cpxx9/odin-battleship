42b23a3456df9daa761b18232226f4b06800d9ff
CPxx9 <127376939+cpxx9@users.noreply.github.com>
2024-05-30 14:55:26 -0400

    test: test hit() method to add 1

--- src/js/tests/Ship.test.js
+++ src/js/tests/Ship.test.js
@@ -2,17 +2,21 @@ import Ship from '../modules/Ship';
 
 describe('instantiation tests', () => {
   test('creates a Ship with length of 3', () => {
-    const sh1 = new Ship(3);
-    expect(sh1.length()).toBe(3);
+    const sh = new Ship(3);
+    expect(sh.length()).toBe(3);
   });
 
   test('creates a Ship with length of 5', () => {
-    const sh2 = new Ship(5);
-    expect(sh2.length()).toBe(5);
+    const sh = new Ship(5);
+    expect(sh.length()).toBe(5);
   });
 });
 
-// describe('hit method tests', () => {
-//   const ship = new Ship
-//   test('')
-// });
+describe('hit method tests', () => {
+  test('hit method adds 1 to timesHit property of Ship', () => {
+    const sh = new Ship(5);
+    sh.hit();
+
+    expect(sh.timesHit()).toBe(1);
+  });
+});
