806319a8c7d7a5f4241a2f8603d69330fcddd07d
CPxx9 <127376939+cpxx9@users.noreply.github.com>
2024-06-03 14:24:22 -0400

    test: able to rotate ship placement

--- src/js/modules/Gameboard.test.js
+++ src/js/modules/Gameboard.test.js
@@ -17,9 +17,36 @@ describe('ship placing tests', () => {
   test('ships take up appropriate space', () => {
     const gb = new Gameboard();
     gb.placeShip(0, 0, 3, 'horizontal');
-    console.log(gb.board);
     expect(gb.board[0][0]).toEqual(new Ship(3));
     expect(gb.board[0][1]).toEqual(new Ship(3));
     expect(gb.board[0][2]).toEqual(new Ship(3));
+  });
+
+  test('ships take up appropriate space v2', () => {
+    const gb = new Gameboard();
+    gb.placeShip(5, 5, 3, 'horizontal');
+    expect(gb.board[5][5]).toEqual(new Ship(3));
+    expect(gb.board[5][6]).toEqual(new Ship(3));
+    expect(gb.board[5][7]).toEqual(new Ship(3));
+  });
+
+  test('ships can be placed rotated', () => {
+    const gb = new Gameboard();
+    gb.placeShip(0, 0, 3, 'verticle');
+    console.log(gb.board);
+
+    expect(gb.board[0][0]).toEqual(new Ship(3));
+    expect(gb.board[1][0]).toEqual(new Ship(3));
+    expect(gb.board[2][0]).toEqual(new Ship(3));
+  });
+
+  test('ships can be placed rotated v2', () => {
+    const gb = new Gameboard();
+    gb.placeShip(5, 5, 3, 'verticle');
+    console.log(gb.board);
+
+    expect(gb.board[5][5]).toEqual(new Ship(3));
+    expect(gb.board[6][5]).toEqual(new Ship(3));
+    expect(gb.board[7][5]).toEqual(new Ship(3));
   });
 });
