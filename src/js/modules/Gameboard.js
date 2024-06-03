bd79dea262f9285be811800cca9fb2aa9325d819
CPxx9 <127376939+cpxx9@users.noreply.github.com>
2024-06-03 13:58:43 -0400

    added rotation param to placeShip method and updated tests

--- src/js/modules/Gameboard.js
+++ src/js/modules/Gameboard.js
@@ -14,10 +14,14 @@ class Gameboard {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
 
-  placeShip(xCoord, yCoord, size) {
+  placeShip(xCoord, yCoord, size, rotation) {
     const ship = new Ship(size);
     for (let i = 0; i < size; i += 1) {
-      this.board[yCoord][xCoord + i] = ship;
+      if (rotation === 'horizontal') {
+        this.board[yCoord][xCoord + i] = ship;
+      } else {
+        this.board[yCoord + i][xCoord] = ship;
+      }
     }
   }
 }
--- src/js/modules/Gameboard.test.js
+++ src/js/modules/Gameboard.test.js
@@ -4,19 +4,19 @@ import Ship from './Ship';
 describe('ship placing tests', () => {
   test('place ship at 0,0 position', () => {
     const gb = new Gameboard();
-    gb.placeShip(0, 0, 3);
+    gb.placeShip(0, 0, 3, 'horizontal');
     expect(gb.board[0][0]).toEqual(new Ship(3));
   });
 
   test('place ship at 5,9 position', () => {
     const gb = new Gameboard();
-    gb.placeShip(5, 9, 2);
+    gb.placeShip(5, 9, 2, 'horizontal');
     expect(gb.board[9][5]).toEqual(new Ship(2));
   });
 
   test('ships take up appropriate space', () => {
     const gb = new Gameboard();
-    gb.placeShip(0, 0, 3);
+    gb.placeShip(0, 0, 3, 'horizontal');
     console.log(gb.board);
     expect(gb.board[0][0]).toEqual(new Ship(3));
     expect(gb.board[0][1]).toEqual(new Ship(3));
