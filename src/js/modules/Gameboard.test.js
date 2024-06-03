5c048f5da4ea3ac4794e3a61d6ce1409ee484edf
CPxx9 <127376939+cpxx9@users.noreply.github.com>
2024-06-03 09:09:31 -0400

    test: more scenarios for plcing ship

--- src/js/modules/Gameboard.test.js
+++ src/js/modules/Gameboard.test.js
@@ -7,4 +7,11 @@ describe('ship placing tests', () => {
     gb.placeShip(0, 0, 3);
     expect(gb.board[0][0]).toEqual(new Ship(3));
   });
+
+  test('place ship at 5,9 position', () => {
+    const gb = new Gameboard();
+    gb.placeShip(5, 9, 2);
+    expect(gb.board[9][5]).toEqual(new Ship(2));
+    console.log(gb.board);
+  });
 });
