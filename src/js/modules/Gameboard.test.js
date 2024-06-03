6640370d98da297410cd8b5a06d06d3ae1ba43f5
CPxx9 <127376939+cpxx9@users.noreply.github.com>
2024-06-03 12:27:49 -0400

    test: ships take up appropriate space

--- src/js/modules/Gameboard.test.js
+++ src/js/modules/Gameboard.test.js
@@ -12,6 +12,14 @@ describe('ship placing tests', () => {
     const gb = new Gameboard();
     gb.placeShip(5, 9, 2);
     expect(gb.board[9][5]).toEqual(new Ship(2));
+  });
+
+  test('ships take up appropriate space', () => {
+    const gb = new Gameboard();
+    gb.placeShip(0, 0, 3);
     console.log(gb.board);
+    expect(gb.board[0][0]).toEqual(new Ship(3));
+    expect(gb.board[0][1]).toEqual(new Ship(3));
+    expect(gb.board[0][2]).toEqual(new Ship(3));
   });
 });
